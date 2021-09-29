1f6647bca52eae914a0ee7efe11b8cd0535b6ae0
Elagabalus <angelsantander609@gmail.com>
2021-09-29 11:39:44 -0500

    Se agrego color a las gráficas de barras

--- Stats.py
+++ Stats.py
@@ -4,7 +4,9 @@ class Stats:
     def __init__(self):
         pass
     def plot(self,resultados:dict):
-        plt.bar(resultados.keys(),[len(palabras) for palabras in resultados.values()])
+        plt.bar(resultados.keys(),
+            [len(palabras) for palabras in resultados.values()],
+            color=['blue','orange','green','purple'])
         plt.ylabel("Palabras", fontsize=12)
         plt.legend()
         plt.grid(True)
