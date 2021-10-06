902be343ce60e40022dcb4f99df3aa9e2c8cfdbe
Elagabalus <angelsantander609@gmail.com>
2021-10-06 12:24:39 -0500

    Agregando fase de entrenamiento

--- Model.py
+++ Model.py
@@ -1,7 +1,12 @@
 # Autor original Donovan Riaño Enriquez, APIT 2021-2
 # Modificado por Ángel Santander
 import pandas as pd
+import os
+from Preprocess import Preprocess
+from TextExtractor import TextExtractor
 
+TRAINING='./training'
+DATA='./data'
 class Model:
     def __init__(self,file='glosario.csv'):
         self.areas=['Area1','Area2','Area3','Area4']
@@ -13,25 +18,47 @@ class Model:
             for area in self.areas:
                    self.words[area]=glosario[area]
 
+    def train(self):
+        try:
+            for area in os.listdir(TRAINING):
+                print(f"Area: {area}")
+                procesador=Preprocess(f"{DATA}/{area}.json")
+                procesador.loadWords()
+                for file in os.listdir(TRAINING+'/'+area):
+                    if file[-3:]=='pdf':
+                        file=TextExtractor(TRAINING+'/'+area+'/'+file)
+                        # text=file.pageRangeText(1,1)
+                        text=file.getAllText()
+                        procesador.countWords(text)
+                procesador.serialize()
+
+
+        except FileNotFoundError:
+            print(f"No se encontró el folder de entrenamiento: {TRAINING}")
+
     def classify(self,text):
         results={area:-1 for area in self.areas}
-        max=(self.areas[0],0)
+        max=('',0)
         if self.file:
             for area in self.areas:
                 results[area]=set(self.words[area]).intersection(text)
 
         for area in results.keys():
             num_matches=len(results[area])
-            if num_matches>0:
-                print(f"{area} Las coincidencias son: {num_matches}")
-                if num_matches>max[1]:
-                    max=(area,num_matches)
-        print(f"El área predominante es {max[0]} con: {max[1]} elementos")
+            # if num_matches>0:
+            print(f"{area} Las coincidencias son: {num_matches} {list(results[area])}")
+            if num_matches>max[1]:
+                max=(area,num_matches)
+        if max[0]!='':
+            print(f"El área predominante es {max[0]} con: {max[1]} elementos")
+        else:
+            print("No se pudo reconocer")
         return results
-    
+
 if __name__=="__main__":
     from TextExtractor import TextExtractor
     model=Model('glosario.csv')
-    file=TextExtractor('java.pdf')
-    text=file.pageRangeText(1,5)
-    model.classify(text)
+    model.train()
+    # file=TextExtractor('java.pdf')
+    # text=file.pageRangeText(1,5)
+    # model.classify(text)
--- TextExtractor.py
+++ TextExtractor.py
@@ -6,12 +6,21 @@ class TextExtractor:
     def __init__(self,filePath:str):
         self.file=filePath
         self.pdf = fitz.open(self.file)
-    def pageRangeText(self,page_a:int,page_b:int):
+    def pageRangeText(self,page_a:int,page_b:int)->list:
         text=''
         for i in range (page_a-1,page_b):
             page = self.pdf.loadPage(i)
             text+= page.getText("text")
         return text.split()
+
+    def getAllText(self):
+        text=''
+        for i in range (0,len(self.pdf)):
+            page = self.pdf.loadPage(i)
+            text+= page.getText("text")
+        return text.split()
+
 if __name__=="__main__":
-    c1=TextExtractor('paper.pdf')
-    print(c1.pageRangeText(1,5).split())
+    c1=TextExtractor('java.pdf')
+    # print(c1.pageRangeText(1,5).split())
+    print(c1.getAllText())
