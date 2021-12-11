e753bf60c32fd8d38adf1de5a277050906bca993
Elagabalus <angelsantander609@gmail.com>
2021-12-10 22:55:46 -0600

    Se agregan la métricas de similitud

--- src/metrics.py
+++ src/metrics.py
@@ -15,26 +15,65 @@ except Exception as e:
 TRAINING_FOLDER=ENV['training']
 VALIDATION_FOLDER=ENV['validation']
 
-model1=ModelConjuntos('glosario.csv')
-model2=Model()
-model3=ModelBayes()
-etiquetas_correctas=[]
-etiquetas_determinadas=[]
-for area in os.listdir(VALIDATION_FOLDER):
-    for file in os.listdir(VALIDATION_FOLDER+os.sep+area):
-        if file[-3:]=='pdf':
-            etiquetas_correctas.append(area)
-            etiqueta_determinada=[-1,-1,-1]
-            pdf=TextExtractor(VALIDATION_FOLDER+'/'+area+'/'+file)
-            text=pdf.getAllText()
-            etiqueta_determinada[0]=model1.classify(text)
-            try:
-                etiqueta_determinada[1]=model2.classify(text)
-            except:
-                print(f"Error: {text}")
-                print(f"arcivo: {file}")
-                exit()
-            etiqueta_determinada[2]=model3.classify(" ".join(text))
-            etiquetas_determinadas.append(etiqueta_determinada)
-print(etiquetas_determinadas)
-print(etiquetas_correctas)
+# model1=ModelConjuntos('glosario.csv')
+# model2=Model()
+# model3=ModelBayes()
+# resultados={}
+# etiquetas_determinadas=[]
+# for area in os.listdir(VALIDATION_FOLDER):
+#     resultados[area]=[]
+#     for file in os.listdir(VALIDATION_FOLDER+os.sep+area):
+#         if file[-3:]=='pdf':
+#             etiqueta_determinada=[-1,-1,-1]
+#             pdf=TextExtractor(VALIDATION_FOLDER+'/'+area+'/'+file)
+#             text=pdf.getAllText()
+#             etiqueta_determinada[0]=model1.classify(text)
+#             etiqueta_determinada[1]=model2.classify(text)
+#             etiqueta_determinada[2]=model3.classify(" ".join(text))
+#             resultados[area].append(etiqueta_determinada)
+# print(etiquetas_determinadas)
+resultados={}
+with open('datos_metricas.json','r') as file:
+    resultados=json.loads(file.read())
+def calcularMetricas(num_modelo):
+    etiquetados_incorrectos={area:0 for area in resultados.keys()}
+    etiquetados_correctos={area:0 for area in resultados.keys()}
+    total=0
+    correctos=0
+    for area in resultados.keys():
+        for etiqueta in resultados[area]:
+            total+=1
+            if etiqueta[num_modelo]==area:
+                etiquetados_correctos[area]+=1
+                correctos+=1
+            else:
+                etiquetados_incorrectos[etiqueta[num_modelo]]+=1
+    print(f"correctos: {correctos} incorrectos: {total-correctos}")
+    print(f"Exactitud {correctos/(total-correctos)}")
+    for area in resultados.keys():
+        # NUMERO DE DOCUMENTOS REALES DE ESA ETIQUETA
+        num_docs=len(resultados[area])
+        # NUMERO DE DOCUMENTOS ETIQUETADOS CON ESTA ETIQUETA
+        num_etiquetados=etiquetados_correctos[area]+etiquetados_incorrectos[area]
+        # NÚMERO DE DOCUMENTOS DE OTRA ETIQETA NO ETIQUETADOS
+        no_etiquetados_correctos=total-num_docs-etiquetados_incorrectos[area]
+        # Métricas
+        exactitud=(etiquetados_correctos[area]+no_etiquetados_correctos)/(total)
+        precision=(etiquetados_correctos[area]/num_docs)
+        if num_etiquetados>0:
+            recall=(etiquetados_correctos[area]/num_etiquetados)
+            f1=2*(precision*recall)/(precision+recall)
+        else:
+            recall=0.0
+            f1=0.0
+        print(area)
+        print(f"Exactitud:{exactitud}")
+        print(f"Precisión:{precision}")
+        print(f"Recall:{recall}")
+        print(f"F1:{f1}")
+print("Métricas método 1")
+calcularMetricas(0)
+print("Métricas método 2")
+calcularMetricas(1)
+print("Métricas método 3")
+calcularMetricas(2)
